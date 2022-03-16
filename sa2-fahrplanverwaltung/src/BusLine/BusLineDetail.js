5adeff45cab82c0c02c152fb7d574ce9781d9185
JonasDenkler <jonasdenkler@gmail.com>
2022-03-16 23:22:40 +0100

    BusLine aufgeräumt

--- sa2-fahrplanverwaltung/src/BusLine/BusLineDetail.js
+++ sa2-fahrplanverwaltung/src/BusLine/BusLineDetail.js
@@ -1,6 +1,6 @@
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import apiService from "../api/ApiService";
 import Header from "../layout/Header";
 
@@ -20,8 +20,7 @@ export default function BusLineDetail({ isStaff, setIsStaff }) {
           <TableHead>
             <TableRow>
               <TableCell key={id}>ID</TableCell>
-              <TableCell>Buslinie</TableCell>
-              <TableCell>Haltestellen</TableCell>
+              <TableCell>Haltestelle</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -29,8 +28,7 @@ export default function BusLineDetail({ isStaff, setIsStaff }) {
               busLineDetail?.lineStops.map((stop) => (
                 <TableRow key={stop.id} className='tablerow'>
                   <TableCell>{stop.id}</TableCell>
-                  <TableCell>{stop.name}</TableCell>
-                  <TableCell>{stop.id}</TableCell>
+                  <TableCell>Name von {stop.id}</TableCell>
                 </TableRow>
               ))
             }
