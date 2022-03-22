853b94969343f04481dc1b5056656adcea4d5ca6
Lars Kecker <lars.kecker2009@gmail.com>
2022-03-22 17:28:31 +0100

    update edit timetable

--- sa2-fahrplanverwaltung/src/TimeTable/EditTimeTable.js
+++ sa2-fahrplanverwaltung/src/TimeTable/EditTimeTable.js
@@ -14,11 +14,30 @@ import { useEffect } from 'react';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
-export default function EditTimeTable({ open, schedule, handleClose, saveSchedule, line, setLine, time, setTime }) {
+export default function EditTimeTable({ open, schedule, handleClose, saveSchedule, line, setLine, time, setTime, setReverseDirection }) {
 
   const [lines, setLines] = useState([]);
 
   useEffect(() => apiService().apiGetAllBusLines().then((result) => setLines(result)), []);
+
+  const [finalStops, setFinalStops] = useState(null);
+  const [selectedFinalStop, setSelectedFinalStop] = useState(null);
+
+  useEffect(() => {
+    if (!line) {
+      return;
+    }
+    apiService().getBusLine(line.id)
+      .then(lineDetail => lineDetail.lineStops)
+      .then(lineStops => {
+        setFinalStops([
+          lineStops[lineStops.length - 1],
+          lineStops[0],
+        ]);
+        setSelectedFinalStop(lineStops[0]);
+      })
+  }, [line])
+
 
   const handleLineChange = (e, newValue) => {
     setLine(newValue);
@@ -27,6 +46,15 @@ export default function EditTimeTable({ open, schedule, handleClose, saveSchedul
   const handleTimeChange = (newValue) => {
     setTime(newValue);
   };
+
+  const handleFinalStopChange = (e, newValue) => {
+    setSelectedFinalStop(newValue);
+    if (newValue === finalStops[1]) {
+      setReverseDirection(true);
+    } else {
+      setReverseDirection(false)
+    }
+  }
 
   return (
     <div>
@@ -46,37 +74,45 @@ export default function EditTimeTable({ open, schedule, handleClose, saveSchedul
             {`Neuen Fahrplan anlegen`}
           </DialogTitle>
         }
-        <DialogContent sx={{ width: 300}}>
+        <DialogContent sx={{ width: 300, display: 'flex', flexDirection: 'column', justifyContent: "space-between" }}>
           {!schedule &&
-          <Autocomplete
-            disablePortal
-            id="combo-box-lines"
-            options={lines}
-            getOptionLabel={(option) => (option ? option.name : "")}
-            onChange={handleLineChange}
-            sx={{ width: 300, marginBottom: 3 }}
-            renderInput={(params) => <TextField {...params} label="Buslinie" />}
-          />
+            <Autocomplete
+              id="combo-box-lines"
+              options={lines}
+              getOptionLabel={(option) => (option ? option.name : "")}
+              onChange={handleLineChange}
+              sx={{ width: 300, marginTop: 2, marginBottom: 3 }}
+              renderInput={(params) => <TextField {...params} label="Buslinie" />}
+            />
           }
           {schedule &&
-          <TextField
-          id="buslinie-read-only-input"
-          label="Buslinie"
-          defaultValue={schedule.line.name}
-          InputProps={{
-            readOnly: true,
-          }}
-          sx={{ width: 300, marginTop : 2, marginBottom: 3}}
-        />
+            <TextField
+              id="buslinie-read-only-input"
+              label="Buslinie"
+              defaultValue={schedule.line.name}
+              InputProps={{
+                readOnly: true,
+              }}
+              sx={{ width: 300, marginTop: 2, marginBottom: 3 }}
+            />
           }
           <LocalizationProvider dateAdapter={AdapterDateFns} >
-          <TimePicker
-            label="Startzeit"
-            value={time}
-            onChange={handleTimeChange}
-            renderInput={(params) => <TextField {...params} />}
-          /> 
+            <TimePicker
+              label="Startzeit"
+              value={time}
+              onChange={handleTimeChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
           </LocalizationProvider>
+          <Autocomplete
+            id="combo-box-final-stops"
+            options={finalStops ?? []}
+            getOptionLabel={stop => stop.busStopName}
+            value={selectedFinalStop}
+            onChange={handleFinalStopChange}
+            sx={{ width: 300, marginTop: 2, marginBottom: 3 }}
+            renderInput={(params) => <TextField {...params} label="Endhaltestelle" />}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} autoFocus>Abbrechen</Button>
